3ecbdce7066d77e0a8c4d1baf85aa757de9493b7
Andrew James Dick <andrewdick1987@gmail.com>
2018-04-25 10:00:38 +0100

    ✨  added colour palette

---
+++ util/colors.js
@@ -0,0 +1,8 @@
+const COLORS = {
+  blue: 30975,
+  orange: 16606720,
+};
+
+module.exports = {
+  COLORS,
+};
--- util/messages.js
+++ util/messages.js
@@ -1,3 +1,5 @@
+const { COLORS } = require('./colors');
+
 const {
   BOT_NAME,
   BOT_PREFIX,
@@ -13,18 +15,18 @@ const MESSAGE_COMMAND_FALLBACK = command =>
   `command **${command}** not recognised. Try **${BOT_PREFIX} ${COMMAND_HELP}** for a list of available commands.`;
 
 const RICH_EMBED_ABOUT = {
-  color: 30975,
+  color: COLORS.blue,
   author: {
     name: `${BOT_NAME} ${BOT_VERSION}`,
     icon_url: `${BOT_ICON_URL}`,
   },
   title: `Something missing? Suggest a feature on GitHub`,
   url: `${BOT_REPO_URL}/issues`,
-  description: `Not what you were looking for? Try **${BOT_PREFIX} ${COMMAND_HELP}** for a list of what's available'.`,
+  description: `Not what you were looking for? Try **${BOT_PREFIX} ${COMMAND_HELP}** for a list of what's available.`,
 };
 
 const RICH_EMBED_HELP = {
-  color: 16606720,
+  color: COLORS.orange,
   title: 'Command List',
   description: `Here is a list of all available commands for ${BOT_NAME}:`,
   fields: [
