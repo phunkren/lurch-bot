7927422d341e8576964de69b1337edc6ce799fe1
Andrew James Dick <andrewdick1987@gmail.com>
2018-05-28 13:58:24 +0100

    🎨 tidied up server logging

--- util/logging.js
+++ util/logging.js
@@ -1,9 +1,9 @@
 const logServerListening = port =>
-  console.log(`[SERVER] server listening on port: ${port}`);
+  console.log(`server listening on port: ${port}`);
 
 const logCommandResponse = message =>
   console.log(
-    `[COMMAND] "${message.content}" requested from ${
+    `"${message.content}" requested from ${
       message.author.username
     } (id: ${message.author.id}) on ${message.channel.guild.name} (id: ${
       message.channel.guild.id
@@ -12,15 +12,12 @@ const logCommandResponse = message =>
 
 const logGuildAdd = guild =>
   console.log(
-    `[INSTALL] New guild joined: ${guild.name} (id: ${
-      guild.id
-    }). This guild has ${guild.memberCount} members!`
+    `New guild joined: ${guild.name} (id: ${guild.id}).
+     This guild has ${guild.memberCount} members!`
   );
 
 const logGuildRemove = guild =>
-  console.log(
-    `[UNINSTALL] I have been removed from: ${guild.name} (id: ${guild.id})`
-  );
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
 
 module.exports = {
   logCommandResponse,
